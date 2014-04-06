0efcfa23c3c1b8bd796e42e12bbf083ebf6b9d04
Josh Carrier <joshjcarrier@gmail.com>
2014-04-06 16:10:08 -0700

    Continuously adds tiles into gameplay.

--- src/console/twentyfortyeight.cpp
+++ src/console/twentyfortyeight.cpp
@@ -10,6 +10,11 @@ public:
         while (true) {
             std::cout << "WASD then <enter> to move: ";
             int keyCode = std::cin.get();
+
+            // ignore subsequent characters so next read is fresh
+            std::cin.clear();
+            std::cin.ignore(INT_MAX,'\n');
+
             if (keyCode == 97) {
                 return MoveDirection::LEFT;
                 break;
@@ -44,39 +49,68 @@ public:
 
 #include <cstdlib>
 
-void print (int board[4][4]) {
-    std::cout << " +---+---+---+---+" << std::endl;
-    for (int y = 0; y < 4; y++) {
-        for (int x = 0; x < 4; x++) {
-            int val = board[y][x] > 0 ? (1 << board[y][x]) : 0;
-            std::cout << " | " << val;
-        }
+class GameBoard {
+private:
+    TileGenerator tileGenerator;
+    int board[4][4] = {{ 0 }}; // initialize board to 0
 
-        std::cout << " |" << std::endl;
-        std::cout << " +---+---+---+---+" << std::endl;
+public:
+    GameBoard() {
+        for (int i = 0; i < 2; i++) {
+            addTile();
+        }
     }
 
-    std::cout << std:: endl;
-}
+    bool addTile() {
+        std::tuple<int, int, int> tile;
+        while (true) {
+            tile = tileGenerator.next();
+            int xPos = std::get<0>(tile);
+            int yPos = std::get<1>(tile);
+
+            if (board[yPos][xPos] > 0) {
+                // TODO check if any spot is possible
+                //return false;
+                continue;
+            }
+
+            break;
+        }
+
+        int xPos = std::get<0>(tile);
+        int yPos = std::get<1>(tile);
+        board[yPos][xPos] = std::get<2>(tile);
+        return true;
+    }
+
+    void print() {
+        std::cout << " +---+---+---+---+" << std::endl;
+        for (int y = 0; y < 4; y++) {
+            for (int x = 0; x < 4; x++) {
+                int val = board[y][x] > 0 ? (1 << board[y][x]) : 0;
+                std::cout << " | " << val;
+            }
+
+            std::cout << " |" << std::endl;
+            std::cout << " +---+---+---+---+" << std::endl;
+        }
+
+        std::cout << std:: endl;
+    }
+};
 
 int main() {
     std::cout << "[ TwentyFortyEight ]" << std::endl;
 
-    int board[4][4];
-    TileGenerator tileGenerator;
-    for (int i = 0; i < 2; i++) {
-        std::tuple<int, int, int> tile = tileGenerator.next();
-        int xPos = std::get<0>(tile);
-        int yPos = std::get<1>(tile);
-        board[yPos][xPos] = std::get<2>(tile);
-    }
+    GameBoard board = GameBoard();
 
     ConsoleMoveDirectionReader directionReader;
 
     while (true) {
-        print(board);
+        board.print();
         MoveDirection direction = directionReader.next();
         std::cout << (int)direction << std::endl;
+        board.addTile();
     }
 
     return 0;
