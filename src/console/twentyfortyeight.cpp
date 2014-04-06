ae824c41153103eec774453969818023c997054d
Josh Carrier <joshjcarrier@gmail.com>
2014-04-06 15:47:59 -0700

    Build and render board.

--- src/console/twentyfortyeight.cpp
+++ src/console/twentyfortyeight.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-//#include <stdio.h>
 
 enum MoveDirection { LEFT, UP, RIGHT, DOWN };
 
@@ -9,6 +8,7 @@ public:
     MoveDirection next() {
 
         while (true) {
+            std::cout << "WASD then <enter> to move: ";
             int keyCode = std::cin.get();
             if (keyCode == 97) {
                 return MoveDirection::LEFT;
@@ -30,12 +30,54 @@ public:
     }
 };
 
+class TileGenerator {
+public:
+    TileGenerator() {
+        std::srand(0);
+    }
+
+    std::tuple<int, int, int> next() {
+        int value = std::rand() % 10 == 0 ? 2 : 1;
+        return std::tuple<int, int, int>(std::rand() % 4, std::rand() % 4, value);
+    }
+};
+
+#include <cstdlib>
+
+void print (int board[4][4]) {
+    std::cout << " +---+---+---+---+" << std::endl;
+    for (int y = 0; y < 4; y++) {
+        for (int x = 0; x < 4; x++) {
+            int val = board[y][x] > 0 ? (1 << board[y][x]) : 0;
+            std::cout << " | " << val;
+        }
+
+        std::cout << " |" << std::endl;
+        std::cout << " +---+---+---+---+" << std::endl;
+    }
+
+    std::cout << std:: endl;
+}
+
 int main() {
-    std::cout << "hello world" << std::endl;
+    std::cout << "[ TwentyFortyEight ]" << std::endl;
+
+    int board[4][4];
+    TileGenerator tileGenerator;
+    for (int i = 0; i < 2; i++) {
+        std::tuple<int, int, int> tile = tileGenerator.next();
+        int xPos = std::get<0>(tile);
+        int yPos = std::get<1>(tile);
+        board[yPos][xPos] = std::get<2>(tile);
+    }
 
     ConsoleMoveDirectionReader directionReader;
-    MoveDirection direction = directionReader.next();
-    std::cout << (int)direction << std::endl;
+
+    while (true) {
+        print(board);
+        MoveDirection direction = directionReader.next();
+        std::cout << (int)direction << std::endl;
+    }
 
     return 0;
 }
