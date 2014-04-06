48c3aff478b6277d58305f92b5d3825fcf25ada1
Josh Carrier <joshjcarrier@gmail.com>
2014-04-06 14:44:13 -0700

    Move direction reader from console.

--- src/console/twentyfortyeight.cpp
+++ src/console/twentyfortyeight.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+//#include <stdio.h>
+
+enum MoveDirection { LEFT, UP, RIGHT, DOWN };
+
+class ConsoleMoveDirectionReader {
+
+public:
+    MoveDirection next() {
+
+        while (true) {
+            int keyCode = std::cin.get();
+            if (keyCode == 97) {
+                return MoveDirection::LEFT;
+                break;
+            }
+            else if (keyCode == 115) {
+                return MoveDirection::DOWN;
+                break;
+            }
+            else if (keyCode == 100) {
+                return MoveDirection::RIGHT;
+                break;
+            }
+            else if (keyCode == 119) {
+                return MoveDirection::UP;
+                break;
+            }
+        }
+    }
+};
 
 int main() {
     std::cout << "hello world" << std::endl;
+
+    ConsoleMoveDirectionReader directionReader;
+    MoveDirection direction = directionReader.next();
+    std::cout << (int)direction << std::endl;
 
     return 0;
 }
